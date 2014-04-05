89b2caec840be56a445b589935edf902ed6a10d7
Aaron Wright <acwrightdesign@gmail.com>
2014-04-05 09:34:10 -0500

    Initial Commit

---
+++ Swatches/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  Swatches
+//
+//  Created by Aaron Wright on 4/5/14.
+//  Copyright (c) 2014 Aaron Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwatchesTests/SwatchesTests.m
@@ -0,0 +1,34 @@
+//
+//  SwatchesTests.m
+//  SwatchesTests
+//
+//  Created by Aaron Wright on 4/5/14.
+//  Copyright (c) 2014 Aaron Wright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwatchesTests : XCTestCase
+
+@end
+
+@implementation SwatchesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
